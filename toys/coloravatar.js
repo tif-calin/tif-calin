cbb91ceeb12ad450355fbf3862d981561dbb860e
culi <culitif@tuta.io>
2021-07-31 19:20:52 -0700

    fix color generation

--- toys/coloravatar.js
+++ toys/coloravatar.js
@@ -1,6 +1,18 @@
 const COLORS = 16777215; // number of colors in rgb integer space
 
-const coloravatar = () => {
+const makeColor = (hash, prime = 211) => {
+  let colorVal = (hash + prime) % 255;
+  let color = '#';
+
+  for (let i = 0; i < 3; i++) {
+    colorVal = (colorVal * prime) % 255;
+    color += (colorVal < 16) ? '0' + colorVal.toString(16) : colorVal.toString(16);
+  }
+
+  return color;
+}
+
+const coloravatar = (loud = false) => {
   const parent = document.createElement('div');
   parent.className = 'color-avatar';
 
@@ -11,22 +23,27 @@ const coloravatar = () => {
   divAva.className = 'avatar';
 
   // add change handler to input
-  inpName.addEventListener('change', e => {
+  inpName.addEventListener('input', e => {
     // remove whitespace from input
     e.target.value = e.target.value.trim();
 
     // turn input into hex color
-    let hash = e.target.value.split('').reduce((acc, char) => acc += char.charCodeAt(0), '');
-    hash = parseInt(hash) % COLORS;
+    let hash = parseInt(e.target.value
+      .split('')
+      .reduce((acc, char) => acc += char.charCodeAt(0), '')
+    ) % 255;
     
-    const deg = '' + (hash % 360);
-    const hex1 = hash.toString(16);
-    const hex2 = ((hash * 8008) % COLORS).toString(16);
+    const deg = (hash % 360);
+
+    if (loud) console.log(deg, makeColor(hash, 71), makeColor(hash, 181));
 
     // update color of avatar
-    document.querySelector('.avatar').style.background = `linear-gradient(${deg}deg, #${hex1}, #${hex2}`;
+    document.querySelector('.avatar').style.background = `linear-gradient(
+      ${deg}deg, 
+      ${makeColor(hash)}, 
+      ${makeColor(hash, 181)}
+    `;
   });
-
 
   // append children and return parent
   parent.append(inpName, divAva);
