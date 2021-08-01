1b74c5cf79ac12c70a1685af7771e5ac08d95fb2
culi <culitif@tuta.io>
2021-07-31 23:50:36 -0700

    hex2rgb

--- utils/utils.js
+++ utils/utils.js
@@ -1,1 +1,3 @@
 export const rgb2hex = rgb => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`;
+
+export const hex2rgb = hex => /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex).map(str => parseInt(str, 16)).slice(1);
