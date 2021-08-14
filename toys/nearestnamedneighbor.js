63b76f67f27ea7b91303f3022edccf775f6bdaec
culi <culitif@tuta.io>
2021-08-13 17:49:30 -0700

    set up for nnn toy

---
+++ toys/nearestnamedneighbor.js
@@ -0,0 +1,16 @@
+
+const handleSubmit = e => {
+  e.preventDefault();
+};
+
+export default options => {
+  const opts = {
+    ...options
+  };
+
+  // create parent element
+  const parent = document.createElement('form');
+
+  // append children and return parent
+  return parent;
+};
