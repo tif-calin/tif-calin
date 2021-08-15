01e0b127ea8cff27d7a97db932239963013c630f
culi <culitif@tuta.io>
2021-08-14 18:27:06 -0700

    add portfolios

---
+++ data/portfolios.js
@@ -0,0 +1,167 @@
+export default [
+	{
+		"name": "Seth Godin",
+		"site": "https://www.sethgodin.com/",
+		"desc": "author, entrepreneur and most of all, a teacher"
+	},
+	{
+		"name": "Evan You",
+		"site": "https://evanyou.me/",
+		"desc": "independent software developer and the creator of the open source javaScript framework vue.js"
+	},
+	{
+		"name": "Sid Shanker",
+		"site": "https://squidarth.com/",
+		"desc": "programmer living in nyc, currently working at twitter, alumni of recurse center"
+	},
+	{
+		"name": "Fred Hebert",
+		"site": "https://ferd.ca/",
+		"desc": "developer and author of some erlang books"
+	},
+	{
+		"name": "John Earnest",
+		"site": "https://beyondloom.com/",
+		"desc": "tinkers with code, makes games and a webcomic"
+	},
+	{
+		"name": "Esteban Herrera",
+		"site": "https://eherrera.net/",
+		"desc": "software developer for over 10 years"
+	},
+	{
+		"name": "Bret Victor",
+		"site": "http://worrydream.com/",
+		"desc": "electrical engineer then programmer then designer then hobo now back"
+	},
+	{
+		"name": "Maggie Appleton",
+		"site": "https://maggieappleton.com/",
+		"desc": "creates visual explanations about design, anthropology, and programming"
+	},
+	{
+		"name": "Sam Newman",
+		"site": "https://samnewman.io/",
+		"desc": "technologist, independent consultant, speaker, author"
+	},
+	{
+		"name": "Alex Korban",
+		"site": "https://korban.net/about/",
+		"desc": "author, software development consultant and once a business co-founder"
+	},
+	{
+		"name": "Yan Zhu",
+		"site": "https://azuki.vip/",
+		"desc": "hacker / dj / producer / artist / ex-physicist"
+	},
+	{
+		"name": "Bert Hubert",
+		"site": "https://berthub.eu/",
+		"desc": "entrepreneur & software developer who sometimes contributes to science"
+	},
+	{
+		"name": "James Magahern",
+		"site": "https://buzzert.net/",
+		"desc": "cyberpunk, pizza freak, arcade rat, operating systems programmer"
+	},
+	{
+		"name": "Cal Paterson",
+		"site": "https://calpaterson.com/",
+		"desc": "contract software engineer, mostly working in python"
+	},
+	{
+		"name": "Dawid Ciężarkiewicz",
+		"site": "https://dpc.pw/",
+		"desc": "long time open source user and contributor, a software engineer by profession and passion"
+	},
+	{
+		"name": "Maxwell Swalding",
+		"site": "https://max.town/",
+		"desc": "functional programmer"
+	},
+	{
+		"name": "Adam Jensen",
+		"site": "https://meissner.rocks/",
+		"desc": "security chief • cyborg • never asked for this"
+	},
+	{
+		"name": "Nelson Heinemann",
+		"site": "https://nelsonheinemann.com/",
+		"desc": "fashion stylist and artist based in munich"
+	},
+	{
+		"name": "Ognjen Regoje",
+		"site": "https://ognjen.io/",
+		"desc": "makes things that run on the web"
+	},
+	{
+		"name": "Samuel Holleran",
+		"site": "https://samholler.com/",
+		"desc": "interdisciplinary artist and researcher"
+	},
+	{
+		"name": "Elizabeth Cantalamessa",
+		"site": "https://www.elizabethcantalamessa.com/",
+		"desc": "phd candidate in philosophy and part-time instructor"
+	},
+	{
+		"name": "Luke Smith",
+		"site": "https://lukesmith.xyz/",
+		"desc": "makes suckless software and a podcast"
+	},
+	{
+		"name": "George Zarkadakis",
+		"site": "https://www.georgezarkadakis.com/",
+		"desc": "writer of fiction and non-fiction, a science communicator, an artificial intelligence engineer, a futurist, and a digital innovation professional"
+	},
+	{
+		"name": "Nate Smith",
+		"site": "https://tilde.town/~vilmibm/",
+		"desc": "computers are pointless unless used to create emotionally compelling and beautiful things"
+	},
+	{
+		"name": "Ryan Dahl",
+		"site": "https://tinyclouds.org/",
+		"desc": "software engineer and the original developer of nodejs and deno"
+	},
+	{
+		"name": "Samay Shamdasani",
+		"site": "https://shamdasani.org/",
+		"desc": "compsci student and builder of enlight.nyc"
+	},
+	{
+		"name": "Dmitrii Gerasimov",
+		"site": "https://beepb00p.xyz/",
+		"desc": "math | physics | programming | music"
+	},
+	{
+		"name": "Gwern Branwen",
+		"site": "https://www.gwern.net/",
+		"desc": "freelance american writer & researcher"
+	},
+	{
+		"name": "Vivek Seth",
+		"site": "https://vivekseth.com/",
+		"desc": "software engineer passionate about using technology to improve peoples’ lives at scale"
+	},
+	{
+		"name": "Chen Nien",
+		"site": "https://www.nien.com/",
+		"desc": "website and mobile app developer, collaborative project participant, product operation and digital marketing expert"
+	},
+	{
+		"name": "Yegor Bugayenko",
+		"site": "https://www.yegor256.com/",
+		"desc": "programmer, founder, investor, and philanthropist"
+	},
+	{
+		"name": "Jason A. Donenfeld",
+		"site": "https://www.zx2c4.com/",
+		"desc": "freelance programming, infrastructure design, security auditing, and technology consulting"
+	},
+	{
+		"name": "Peter Hajas",
+		"site": "http://peterhajas.com/",
+		"desc": "software developer at apple"
+	}
+];
